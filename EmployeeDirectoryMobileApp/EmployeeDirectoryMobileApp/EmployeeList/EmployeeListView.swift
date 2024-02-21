2b84eab8626f97f93bb13816baa31921017c9c83
Eric Palma <palmae95@gmail.com>
2024-02-21 08:16:51 -0500

    add Employee List View

---
+++ EmployeeDirectoryMobileApp/EmployeeDirectoryMobileApp/EmployeeList/EmployeeListView.swift
@@ -0,0 +1,20 @@
+//
+//  EmployeeListView.swift
+//  EmployeeDirectoryMobileApp
+//
+//  Created by Eric Palma on 2/21/24.
+//
+
+import SwiftUI
+
+struct EmployeeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmployeeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmployeeListView()
+    }
+}
