be88166cdbf3508015194404806eeef8d2ec0933
Eric Palma <palmae95@gmail.com>
2024-02-22 09:15:15 -0500

    employee list view pushes detail view

--- EmployeeDirectoryMobileApp/EmployeeDirectoryMobileApp/EmployeeList/EmployeeListView.swift
+++ EmployeeDirectoryMobileApp/EmployeeDirectoryMobileApp/EmployeeList/EmployeeListView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 import EmployeeDirectoryCore
 
 struct EmployeeListView: View {
-    let useCase: FetchEmployeeListUseCase
-    @State var employees: [Employee] = []
+    private let repository: EmployeesRepository
+    private let useCase: FetchEmployeeListUseCase
+    @State private var employees: [Employee] = []
+    
+    init(repository: EmployeesRepository) {
+        self.repository = repository
+        useCase = FetchEmployeeListUseCase(repository: repository)
+    }
     
     var body: some View {
-        List(employees) { employee in
-            Text(employee.firstName + " " + employee.lastName)
+        NavigationStack {
+            List(employees) { employee in
+                NavigationLink(employee.firstName + " " + employee.lastName, value: employee.id)
+            }
+            .navigationDestination(for: Employee.Identifier.self) { id in
+                EmployeeDetailsView(repository: repository, employeeID: id)
+            }
         }
         .task {
             guard let employeesResult = try? await useCase.execute() else { return }
